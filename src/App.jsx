0cdd8cd47410a03248f1c4418175d72a6773ace0
HarreshJ <harresh@hotmail.com>
2024-07-05 14:44:19 -0400

    adding the right files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function App() {
+  const [isTrue, setIsTrue] = useState(false);
+
+  function onClick() {
+    setIsTrue((value) => !value)
+  }
+
+  return (
+    <div>
+      Foo
+      <button onClick={onClick}>Click me</button>
+      {isTrue ?       
+      <h1>Badman Shepard Travel Award Project Website</h1>
+      : <h1>Harresh Travel Award Project Website</h1>}
+    </div>
+  )
+}
+
+export default App
