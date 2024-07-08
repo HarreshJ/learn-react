aa64e782d22c1069f89265ad4928499177add930
HarreshJ <harresh@hotmail.com>
2024-07-07 21:10:57 -0400

    making the grid

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
+import 'styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const button_pressed = false
+
+const GridComponent = () => {
+  const handleClick = () => {
+      console.log(`Button clicked`);
+      // Add your desired functionality here
+      Alert.alert("A button has been pressed")
+
+  };
+
+  return (
+      <div className="grid-container">
+          <button className="column1" onClick={() => handleClick()}></button>
+          <button className="column1" onClick={() => handleClick()}></button>
+          <button className="column1" onClick={() => handleClick()}></button>
+          <button className="column2" onClick={() => handleClick()}></button>
+          <button className="column2" onClick={() => handleClick()}></button>
+          <button className="column2" onClick={() => handleClick()}></button>
+          <button className="column3" onClick={() => handleClick()}></button>
+          <button className="column3" onClick={() => handleClick()}></button>
+      </div>
+  );
+}
